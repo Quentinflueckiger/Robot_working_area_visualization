bb457c1360fdd5815f42e9d3da050884235d5672
Quentin <quentin.flueckiger@hispeed.ch>
2018-10-09 13:11:16 +0200

    First animation
    Some interesting problem

---
+++ Robot_working_area_visualisation/Assets/Scripts/KukaBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class KukaBehaviour : MonoBehaviour {
+
+    private Animator _animator;
+	// Use this for initialization
+	void Start () {
+
+        _animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("space"))
+            _animator.SetTrigger("Grab_Left2");
+	}
+}
