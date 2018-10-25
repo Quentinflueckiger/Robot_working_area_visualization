5469e03a14ec04b93401d8baaf2fce07d008b607
Quentin <quentin.flueckiger@hispeed.ch>
2018-10-25 16:18:31 +0200

    Finish animation
    Animation chosen randomly, can do a sequence of animation.

--- Robot_working_area_visualisation/Assets/Scripts/KukaBehaviour.cs
+++ Robot_working_area_visualisation/Assets/Scripts/KukaBehaviour.cs
@@ -1,20 +1,43 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class KukaBehaviour : MonoBehaviour {
 
+    /*
     public Collider baseCollider;
     public Collider bicepsCollider;
     public Collider forearmCollider;
-    public Collider handCollider;
+    public Collider handCollider;*/
+
+    public Button animateButton;
+    public Slider animateSlider;
+
+    [Space(10)]
+    [Header("Position Helper")]
+    public GameObject basePositionHelper;
+
+    public GameObject bicepsPositionHelperFirst;
+    public GameObject bicepsPositionHelperSecond;
+
+    public GameObject foreArmPositionHelperFirst;
+    public GameObject foreArmPositionHelperSecond;
+    public GameObject foreArmPositionHelperThird;
+
+    public GameObject handPositionHelper;
 
     private Animator _animator;
 
+    private bool status =false;
     private int randomIndex;
-    private Quaternion baseForearmRotation;
     private bool isInAnimation;
     private bool isInQueue;
+    private static Stack<int> randomQueue = new Stack<int>();
+    private int frameCounter = 0;
+
+    private Vector3 foreArmPositionHelperSecondOld;
 
     // Use this for initialization
     void Start () {
@@ -22,28 +45,124 @@ public class KukaBehaviour : MonoBehaviour {
         isInAnimation = false;
         isInQueue = false;
         _animator = GetComponent<Animator>();
+        foreArmPositionHelperSecondOld = foreArmPositionHelperSecond.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
         
+        // Play a random animation on space pressed
         if (Input.GetKey("space"))
         {
 
             if (!isInAnimation && !isInQueue)
-                AnimateRandomly();
+                Animate();
         }
-        DrawDebuggRay();
 
-      /* else if (isInQueue)
+        // If there are something in the stack execute this part
+        else if (isInQueue)
         {
-            // put the multiple animation here
-        }*/
+            // Test if we are already playing an animation
+            // the second part is used to wait because I encounter some problem if I wasn't waiting between two animations
+           if (!isInAnimation && frameCounter % 3 == 0)
+           {
+               
+                if (randomQueue.Count == 0)
+                {
+                    isInQueue = false;
+                    ButtonSliderChangeVisibility();
+                }
+                    
+                else
+                {
+                    Animate(randomQueue.Pop()); 
+                }
+                    
+            }
+        }
+
+        if (frameCounter == 60)
+            frameCounter = 1;
+        else
+            frameCounter++;
+    }
+
+    private void LateUpdate()
+    {
+        if (isInAnimation)
+        {
+            DrawDebuggRay();
+        }
+    }
+
+    /** This method is used to prepare a stack filled with random int.
+     *  These int will be used to play animation in a sequence.
+     *  The amount of random number is based on the animationSlider's value.
+     * 
+     */
+    public void PrepareSequenceOfRandomAnimation()
+    {
+
+        isInQueue = true;
+        int numberOfAnimation = (int)animateSlider.value;
+        
+        for (int i = 0; i < numberOfAnimation; i++)
+        {
+            randomQueue.Push(UnityEngine.Random.Range(0, 12));
+        }
+        ButtonSliderChangeVisibility();
+
+    }
+
+    /** Set the trigger for the animator with a random int.
+     * 
+     */
+    private void Animate()
+    {
+        isInAnimation = true;
+        randomIndex = UnityEngine.Random.Range(0, 12);
+        _animator.SetInteger("AnimParam", randomIndex);
+    }
+
+    /** Set the trigger for the animator based on an input int.
+     * 
+     * @animationNumber the int used for the trigger
+    */
+    private void Animate(int animationNumber)
+    {
+        isInAnimation = true;
+        _animator.SetInteger("AnimParam", animationNumber);
+    }
+
+    /** Reset the Animator Parameter at the end of the animation.
+     * 
+     */   
+    public void AnimationEnded()
+    {
+        _animator.SetInteger("AnimParam", 0);
+        isInAnimation = false;
+    }
+
+    /** Switch the slider and the button's visibility.
+     * 
+     */ 
+    private void ButtonSliderChangeVisibility()
+    {
+        animateButton.gameObject.SetActive(status);
+        animateSlider.gameObject.SetActive(status);
+        status = !status ;
     }
 
     private void DrawDebuggRay()
     {
- 
+
+        Debug.Log("Vector diff " + (foreArmPositionHelperSecond.transform.position - foreArmPositionHelperSecondOld));
+        Debug.DrawLine(foreArmPositionHelperSecond.transform.position, 
+                foreArmPositionHelperSecond.transform.position + 
+                (foreArmPositionHelperSecond.transform.position - foreArmPositionHelperSecondOld),
+                Color.red);
+        if (frameCounter % 2 == 0)
+            foreArmPositionHelperSecondOld = foreArmPositionHelperSecond.transform.position;
         /*
         Debug.DrawLine(baseCollider.transform.position + (Vector3.up * 0.1f),
                 baseCollider.transform.position + (Vector3.up * 0.1f) +
@@ -61,55 +180,6 @@ public class KukaBehaviour : MonoBehaviour {
         Debug.DrawLine(handCollider.transform.position + (Vector3.up * 0.1f),
                 handCollider.transform.position + (Vector3.up * 0.1f) +
                 (Vector3.down * 1), Color.red);*/
-    }
 
-    public void PrepareSequenceOfRandomAnimation(Slider numberOfAnimationSlider)
-    {
-        if (!isInQueue)
-        {
-            isInQueue = true;
-            int numberOfAnimation = (int)numberOfAnimationSlider.value;
-            int[] animationQueue = new int[numberOfAnimation];
-
-            Debug.Log("Input = " + numberOfAnimation);
-            for (int i = 0; i < numberOfAnimation; i++)
-            {
-                animationQueue[i] = UnityEngine.Random.Range(0, 12);
-            }
-
-            MultipleAnimation(animationQueue);
-        }
-        
-    }
-
-    private void MultipleAnimation(int[] animationQueue)
-    {
-        for (int i = 0; i < animationQueue.Length; i++)
-        {
-            isInAnimation = true;
-            Debug.Log("I'm in here !");
-            _animator.SetInteger("AnimParam", animationQueue[i]);
-        }
-       
-        isInQueue = false;
-    }
-
-    private void AnimateRandomly()
-    {
-        isInAnimation = true;
-        randomIndex = UnityEngine.Random.Range(0, 12);
-        _animator.SetInteger("AnimParam", randomIndex);
-    }
-
-    // Reset the Animator Parameter at the end of the animation
-    public void AnimationEnded()
-    {
-        _animator.SetInteger("AnimParam", 0);
-        isInAnimation = false;
-    }
-
-    public bool getIsInQueue()
-    {
-        return isInQueue;
     }
 }
---
+++ Robot_working_area_visualisation/Assets/Scripts/SetTextValue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class SetTextValue : MonoBehaviour {
+
+    public Text sliderValue;
+    public Slider slider;
+
+    void Update()
+    {
+
+        sliderValue.text = slider.value.ToString("0");
+    }
+}
